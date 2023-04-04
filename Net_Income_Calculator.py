e36feefdcca644efdcdbcfa4bb905dea05270592
Aduboahen <oseimensa@outlook.com>
2023-04-04 14:16:49 +0000

    PAYE Net Income Calculator Ghana

--- Net_Income_Calculator.py
+++ Net_Income_Calculator.py
@@ -1,19 +1,51 @@
-basic = float(input("What is your basic salary? : \n"))
+while True:
+    try:
+        basic = float(input("What is your basic salary? : "))
+        break
+    except ValueError:
+        print("Please enter a number.")
 
-#Additions
-risk_allowance = (0.3 * basic)
-overtime_allowance = (0.15 * basic)
+# Additions
 
-#Subtractions
-ssnit = (0.055 * basic)
-providence = (float(input("Percentage contributed to Providence Insurance : \n"))/100) * basic
+while True:
+    try:
+        allowances = float(
+            input("Enter the sum of all allowances received: ")) / 100 * basic
+        break
+    except ValueError:
+        print("Please enter a number.")
 
-first_132 = (basic - 132)
-next_66 = (first_132) * 0.05
-next_92 = (first_132 - next_66) * 0.1
+gross = basic + allowances
 
-income_tax =  (next_66 + next_92 + first_32)
-      
-net_income =  basic + (risk_allowance + overtime_allowance) - (ssnit+ income_tax + providence)
+# deductions
 
-print(f"This is your Net Income(Take Home Salary) \n {net_income}")
+ssnit_pf: float = (0.05 * basic) + (0.055 * basic)
+
+taxable_income = gross - ssnit_pf
+
+# income tax
+
+income_tax = 0
+
+chargeable_income = 365
+
+chargeable_income_d = {110: 0.05, 130: 0.1,
+                       3_000: 0.175, 16_395: 0.25, 20_000: 0.3}
+
+old_key = 365
+
+key: int
+for key, value in chargeable_income_d.items():
+    if 0 < taxable_income - chargeable_income >= key:
+        income_tax += value * key   
+        chargeable_income += key
+
+    elif key > taxable_income - chargeable_income > old_key:
+        income_tax += value * (taxable_income - chargeable_income)
+    old_key = key
+
+print(f"\nallowances = {allowances}\nincome_tax = {income_tax}")
+
+net_income: float = basic + allowances - ssnit_pf - income_tax
+
+print(f"\nThis is your Net Income(Take Home Salary)\n{net_income}")
